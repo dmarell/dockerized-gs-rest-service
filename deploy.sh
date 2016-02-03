50599aa92441a634c0d0d6f44734492b5f563613
KDMA02 <daniel.marell@gmail.com>
2016-02-03 14:20:26 +0100

    Port.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ docker build -t gs-rest-service .
 docker rm -f gs-rest-service &> /dev/null || true
 docker run \
     -d --name gs-rest-service \
-    -p 8080:8080 \
+    -p 10201:8080 \
     gs-rest-service
