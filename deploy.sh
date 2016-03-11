3e921ca8ccd9f476345d40b0c06b8ba85ab31ce0
Daniel Marell <daniel.marell@gmail.com>
2016-03-11 09:02:03 +0100

    Dockerizing adjustments

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-docker build -t gs-rest-service .
-docker rm -f gs-rest-service &> /dev/null || true
+docker build -t gs-rest-service-10201 .
+docker rm -f gs-rest-service-10201 &> /dev/null || true
 docker run \
-    -d --name gs-rest-service \
-    -p 10201:8080 \
-    gs-rest-service
+   -d --name gs-rest-service-10201 \
+   -p 10201:8080 \
+   gs-rest-service-10201
